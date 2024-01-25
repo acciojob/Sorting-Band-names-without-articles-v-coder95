4c1f176e7b9b45cee89e13e6a38b524259916d1e
poonam kushwaha <134260040+v-coder95@users.noreply.github.com>
2024-01-25 07:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+let arr=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+ let store= arr.sort((a,b)=>{
+     return (split(a)>split(b)?1:-1);
+ });
+ function split(name){
+     let reg = new RegExp('^(a|an|the)','i');
+     return name.replace(reg,'').trim();
+ }
+
+let ul = document.getElememtById("band");
+ul.innerHTML = store.map((item)=>{`<li>${item}</li>`});
+
+
 //your code here
 
